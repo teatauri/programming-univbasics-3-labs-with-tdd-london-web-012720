c427f8154c5db4d29996eccf2cf5bfa6a4e97551
Lee Bardon <leerbardon@gmail.com>
2019-12-14 13:39:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@
 first_number = 10
 second_number = 3
 sum = first_number + second_number
-difference = first_number - second_number
+difference = first number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
